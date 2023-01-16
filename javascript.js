b4ed5a46842de260505d9abefce5d723bfebe8c7
Chris <hakizachrist1@gmail.com>
2023-01-16 23:33:28 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,15 +2,16 @@ let library = [];
 
 function getInput() {
   const book = {};
+  book.id = Date.now();
   book.title = document.getElementById('bookTitle').value;
   book.author = document.getElementById('bookAuthor').value;
   return book;
 }
 
-function removeBook(title) {
-  const book = document.getElementById(title);
+function removeBook(id) {
+  const book = document.getElementById(id);
   book.remove();
-  library = library.filter((bookObj) => bookObj.title !== title);
+  library = library.filter((bookObj) => bookObj.id !== id);
   localStorage.setItem('library', JSON.stringify(library));
 }
 
@@ -18,11 +19,11 @@ function addBook(bookObj) {
   const bookList = document.getElementById('book-list');
   const book = document.createElement('li');
   const line = document.createElement('hr');
-  book.setAttribute('id', bookObj.title);
+  book.setAttribute('id', bookObj.id);
   book.innerHTML = `<h2 class="title"> ${bookObj.title} </h2> <h3>${bookObj.author} </h3>`;
   const deleteBtn = document.createElement('button');
   deleteBtn.innerHTML = 'Remove';
-  deleteBtn.addEventListener('click', () => removeBook(bookObj.title));
+  deleteBtn.addEventListener('click', () => removeBook(bookObj.id));
   book.appendChild(deleteBtn);
   book.appendChild(line);
   bookList.appendChild(book);
